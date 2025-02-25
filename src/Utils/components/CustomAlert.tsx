b3bbe1d1a3e14891f4a464ab751590040a30aa3a
AlexPalinya <ai3n17a75@gmail.com>
2025-02-25 11:32:16 +0700

    .

---
+++ src/Utils/components/CustomAlert.tsx
@@ -0,0 +1,115 @@
+import React from 'react';
+import {Modal, View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import color from '../../Constants/color';
+
+const theme = 'light';
+interface CustomAlertProps {
+  visible: boolean;
+  title: string;
+  message: string;
+  onCancel: () => void;
+  onConfirm: () => void;
+}
+
+const CustomAlert: React.FC<CustomAlertProps> = ({
+  visible,
+  title,
+  message,
+  onCancel,
+  onConfirm,
+}) => {
+  return (
+    <Modal visible={visible} transparent animationType="fade">
+      <View style={styles.overlay}>
+        <View style={styles.alertBox}>
+          {/* ปุ่มปิด */}
+          <TouchableOpacity style={styles.closeButton} onPress={onCancel}>
+            <Text style={styles.closeText}>✕</Text>
+          </TouchableOpacity>
+
+          <Text style={styles.title}>{title}</Text>
+          <Text style={styles.message}>{message}</Text>
+
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity style={styles.cancelButton} onPress={onCancel}>
+              <Text style={styles.cancelText}>ຍົກເລີກ</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.confirmButton} onPress={onConfirm}>
+              <Text style={styles.confirmText}>ຕົກລົງ</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </Modal>
+  );
+};
+
+export default CustomAlert;
+
+const styles = StyleSheet.create({
+  overlay: {
+    flex: 1,
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  alertBox: {
+    width: 320,
+    backgroundColor: 'white',
+    borderRadius: 10,
+    padding: 20,
+    alignItems: 'center',
+    position: 'relative',
+  },
+  closeButton: {
+    position: 'absolute',
+    top: 10,
+    right: 10,
+    padding: 5,
+  },
+  closeText: {
+    fontSize: 20,
+    color: '#777',
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 30,
+    textAlign: 'center',
+  },
+  message: {
+    fontSize: 16,
+    textAlign: 'center',
+    marginBottom: 40,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+    // marginBottom: 10,
+    gap: 15,
+  },
+  cancelButton: {
+    flex: 1,
+    backgroundColor: '#ccc',
+    paddingVertical: 10,
+    borderRadius: 5,
+    marginRight: 10,
+    alignItems: 'center',
+  },
+  confirmButton: {
+    flex: 1,
+    backgroundColor: color[theme].primary,
+    paddingVertical: 10,
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+  cancelText: {
+    fontSize: 16,
+    color: '#333',
+  },
+  confirmText: {
+    fontSize: 16,
+    color: '#fff',
+  },
+});
